314d8fde66b9a2de0bf8683a450b32fdcf1855e0
ATolkachev <tolkachev75@gmail.com>
2018-01-19 19:14:57 +0400

    added code to the GroupAddController class

--- src/control/view/ContactsGroupAddDialogController.java
+++ src/control/view/ContactsGroupAddDialogController.java
@@ -1,4 +1,28 @@
 package control.view;
 
+import control.model.PersonContactGroup;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class ContactsGroupAddDialogController {
+    private Stage dialogStage;
+    private PersonContactGroup personContactGroup;
+    private boolean isAddClicked;
+
+    @FXML
+    private TextField groupName;
+
+    @FXML
+    private void initialize() {
+    }
+
+    /**
+     * Установка главной сцена для диалогового окна
+     *
+     * @param stage - Stage в MainApp классе
+     */
+    public void setPrimaryStage(Stage stage) {
+        this.dialogStage = stage;
+    }
 }
